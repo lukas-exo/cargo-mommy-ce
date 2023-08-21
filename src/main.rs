5e6fe1e7ce460a60d32fd8667b21a8524fbb4480
Lukas Friedrich <lukasfriedrich@mail.de>
2023-08-21 14:25:52 +0200

    Revert "Faster Mommy"
    This reverts commit f0fab6f2e4babdcad9ce08cc235801a87fc47f63.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 #![allow(clippy::let_and_return)]
 
-<<<<<<< HEAD
 use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
-=======
-use fastrand::Rng;
-use serde::Deserialize;
->>>>>>> 6ae19f83e8bb866d72392a958515b32aea1621c3
 
 mod config;
 use config::{load_config, Responses};
@@ -49,13 +44,9 @@ fn real_main() -> Result<i32, Box<dyn std::error::Error>> {
 }
 
 fn select_response(response_type: ResponseType) -> String {
-<<<<<<< HEAD
     let config = load_config().unwrap();
 
     let mut rng = StdRng::from_entropy();
-=======
-    let rng = Rng::new();
->>>>>>> 6ae19f83e8bb866d72392a958515b32aea1621c3
 
     // Get mommy's options~
     let affectionate_terms = parse_options(config.affectionate_terms);
@@ -65,7 +56,6 @@ fn select_response(response_type: ResponseType) -> String {
     // Choose what mommy will say~
     let responses: Responses = config.responses.unwrap_or_default();
 
-<<<<<<< HEAD
     let responses_binding = match response_type {
         ResponseType::Positive => responses.positive,
         ResponseType::Negative => responses.negative,
@@ -74,28 +64,21 @@ fn select_response(response_type: ResponseType) -> String {
     let response = responses_binding
         .choose(&mut rng)
         .expect("non-zero amount of responses");
-=======
-    let responses = match response_type {
-        ResponseType::Positive => &responses.positive,
-        ResponseType::Negative => &responses.negative,
-    };
-    let response = &responses[rng.usize(..responses.len())];
->>>>>>> 6ae19f83e8bb866d72392a958515b32aea1621c3
 
     // Apply options to the message template~
     let response = apply_template(
         response,
         AFFECTIONATE_TERM_PLACEHOLDER,
         &affectionate_terms,
-        &rng,
+        &mut rng,
     );
     let response = apply_template(
         &response,
         MOMMYS_PRONOUN_PLACEHOLDER,
         &mommys_pronouns,
-        &rng,
+        &mut rng,
     );
-    let response = apply_template(&response, MOMMYS_ROLE_PLACEHOLDER, &mommys_roles, &rng);
+    let response = apply_template(&response, MOMMYS_ROLE_PLACEHOLDER, &mommys_roles, &mut rng);
 
     // Done~!
     response
@@ -105,12 +88,12 @@ fn parse_options(input: String) -> Vec<String> {
     input.split('/').map(|s| s.to_string()).collect()
 }
 
-fn apply_template(input: &str, template_key: &str, options: &[String], rng: &Rng) -> String {
+fn apply_template(input: &str, template_key: &str, options: &[String], rng: &mut StdRng) -> String {
     let mut last_position = 0;
     let mut output = String::new();
     for (index, matched) in input.match_indices(template_key) {
         output.push_str(&input[last_position..index]);
-        output.push_str(&options[rng.usize(..options.len())]);
+        output.push_str(options.choose(rng).unwrap());
         last_position = index + matched.len();
     }
     output.push_str(&input[last_position..]);
